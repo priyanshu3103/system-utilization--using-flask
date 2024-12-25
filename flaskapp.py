ccc48cc44ea4ad60f317a2bcaf70c2e6a555a96a
Priyanshu Barapatre <101656280+priyanshu3103@users.noreply.github.com>
2024-12-25 15:57:05 +0530

    Update flaskapp.py

--- flaskapp.py
+++ flaskapp.py
@@ -11,7 +11,7 @@ def index():
     mem_percent = psutil.virtual_memory().percent
     message = None
     if cpu_percent > 80 or mem_percent > 80:
-        message = "High CPU or memory utilization detected!!"
+        message = "High CPU or memory utilization detected!"
     return render_template("index.html", cpu_percent=cpu_percent, mem_percent=mem_percent, message=message)
 
 if __name__ == '__main__':
